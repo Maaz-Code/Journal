4104cbae360776ad29b49c72b8239a7439b77d05
Maaz-Code <500077097@stu.upes.ac.in>
2021-09-26 21:26:06 +0530

    Adding AppBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import Home from './components/Home';
 import "./styles.scss";
+import Appbar from './components/Appbar';
 
 const App = () => {
   return (
     <>
+      <Appbar/>
       <Home/>
     </>
   );
---
+++ frontend/src/components/Appbar.jsx
@@ -0,0 +1,86 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+import { styled, alpha } from '@mui/material/styles';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
+
+
+export default function Appbar() {
+  return (
+    <Box sx={{ flexGrow: 1}}>
+      <AppBar position="static" sx={
+        { 
+          backgroundColor: '#282C35', 
+          borderEndStartRadius: '1.5rem',
+          borderEndEndRadius: '1.5rem'
+        }
+      }>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, ml: 2, display: {xs:'none', sm:'block'} }}>
+            Journal
+          </Typography>
+          <Search sx={{mr: 2, display: {xs:'none', sm:'block'}}}>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
+          <IconButton size="large" aria-label="search" color="inherit" >
+            <SearchIcon sx={{display: {xs:'block', lg:'none'}}} />
+          </IconButton>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
